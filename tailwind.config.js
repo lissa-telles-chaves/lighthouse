532fb91afb9f07703bfdaa0b4e8999a770dc1250
Lissa Telles Chaves <lissatchaves@gmail.com>
2026-04-26 23:08:40 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#2C5F8A',
+      },
+    },
+  },
+  plugins: [],
+};
